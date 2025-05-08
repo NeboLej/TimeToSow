68f8304a9412107a4537bb2b653d7c5336e2ff9e
Nebo <kindersmail08@gmail.com>
2025-05-08 16:57:15 +0300

    init commit

---
+++ TimeToSow/TimeToSowApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeToSowApp.swift
+//  TimeToSow
+//
+//  Created by Nebo on 08.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct TimeToSowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
