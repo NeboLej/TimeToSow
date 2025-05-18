43d71d4f6fc7026304560589c5170f290e8a2e20
Nebo <kindersmail08@gmail.com>
2025-05-18 19:27:52 +0300

    added element picker view firs version

---
+++ TimeToSow/UI/Components/StrokeText.swift
@@ -0,0 +1,89 @@
+import SwiftUI
+import CoreText
+
+struct TextEllipseStrokeView: View {
+    let text: String
+    let font: Font = .system(size: 18)
+    let duration: Double = 0.5
+    
+    @State var startTime: Date? = .now
+    var isSelected: Bool
+    
+    var body: some View {
+        ZStack {
+            GeometryReader { geometry in
+                let size = geometry.size
+                let ellipsePath = createHandDrawnEllipsePath(for: size)
+
+                if isSelected, startTime != nil {
+                    TimelineView(.animation) { timeline in
+                        let currentTime = timeline.date.timeIntervalSince(startTime!)
+                        let progress = min(currentTime / duration, 1.0)
+
+                        Canvas { context, _ in
+                            let trimmedPath = ellipsePath.trimmedPath(from: 0, to: progress)
+                            context.stroke(
+                                trimmedPath,
+                                with: .color(.strokeAcsent),
+                                lineWidth: 2.0
+                            )
+                        }
+                    }
+                }
+
+            }
+
+
+            Text(text)
+                .font(font)
+                .multilineTextAlignment(.center)
+                .padding()
+                .onChange(of: isSelected) { oldValue, newValue in
+                    if newValue {
+                        startTime = .now
+                    }
+                }
+        }
+//        .frame(width: 250, height: 100)
+
+    }
+
+    private func createHandDrawnEllipsePath(for size: CGSize) -> Path {
+        let boundingRect = CGRect(
+            x: 0,
+            y: 0,
+            width: size.width,
+            height: size.height
+        ).insetBy(dx: 5, dy: 8)
+
+        var path = Path()
+        let steps = 20
+        let noiseAmount: CGFloat = 2.0
+
+        for i in 0..<steps { // Avoid closing the path
+            let angle = CGFloat(i) / CGFloat(steps - 1) * 2 * .pi
+            let x = boundingRect.midX + boundingRect.width / 2 * cos(angle)
+            let y = boundingRect.midY + boundingRect.height / 2 * sin(angle)
+
+            let randomX = x + CGFloat.random(in: -noiseAmount...noiseAmount)
+            let randomY = y + CGFloat.random(in: -noiseAmount...noiseAmount)
+
+            if i == 0 {
+                path.move(to: CGPoint(x: randomX, y: randomY))
+            } else {
+                path.addLine(to: CGPoint(x: randomX, y: randomY))
+            }
+        }
+
+        return path // Do not close the subpath
+    }
+}
+
+//#Preview {
+//    @Previewable @State var selectedElement: PickerElement = .new
+//    SelectPickerView(selectedElement: $selectedElement)
+//}
+
+#Preview {
+    HomeScreen()
+}
--- TimeToSow/UI/HomeScreen/HomeScreen.swift
+++ TimeToSow/UI/HomeScreen/HomeScreen.swift
@@ -13,7 +13,8 @@ struct HomeScreen: View {
     @Environment(\.screenBuilder) var screenBuilder: ScreenBuilder
     
     @State var activityType: Int = 0
-    @State var selectedTime: Int = 10
+    @State var selectedTime: Int = 50
+    @State var selectedElement: PickerElement = .new
     
     var body: some View {
         ZStack {
@@ -22,8 +23,6 @@ struct HomeScreen: View {
                 newPlantSection()
                     .offset(y: -20)
                 Group {
-                    
-//                        .frame(height: 220)
                     Rectangle()
                         .frame(height: 300)
                     Rectangle()
@@ -42,63 +41,41 @@ struct HomeScreen: View {
     func newPlantSection() -> some View {
         
         VStack {
-            HStack {
+            HStack(spacing: 0) {
                 VStack {
                     Text("New Plant")
+                        .font(.system(size: 30))
+                        .padding(.top, 24)
+                    Spacer()
                     Text("\(selectedTime) min")
+                        .font(.system(size: 30))
+                    Spacer()
                     Button {
                         print("start")
                     } label: {
                         Text("Start")
+                            .font(.system(size: 30))
                     }
                 }
                 VStack {
-                    activityPicker()
-                        .padding(.horizontal, 30)
+                    SelectPickerView(selectedElement: $selectedElement)
+                        .padding(.horizontal, 0)
                     Image(ImageResource.plantActivity)
                         .offset(x: 10)
                 }
-                .padding(.vertical, 16)
+                .padding(.all, 16)
             }
             TimePickerView(selectedTime: $selectedTime)
                 .frame(height: 100)
                 .padding(.horizontal, 8)
         }
+        .padding(.horizontal, 12)
         .frame(maxWidth: .infinity)
         .background {
             Image(.sectionBackground)
                 .resizable()
         }
-        
-//        Rectangle()
-//            .frame(height: 320)
-//            .overlay {
-//                Image(.sectionBackground)
-//                    .resizable()
-//            }
-        
-//        Image(.sectionBackground)
-//            .resizable()
-//            .aspectRatio(contentMode: .fit)
-//            .background(.black.opacity(0.5))
-            
     }
-    
-    @ViewBuilder
-    private func activityPicker() -> some View {
-        Picker(selection: $activityType) {
-            Text("build").tag(0)
-            Text("greening").tag(1)
-        } label: { }
-            .pickerStyle(.segmented)
-            .onAppear {
-                UISegmentedControl.appearance().backgroundColor = .white
-                UISegmentedControl.appearance().selectedSegmentTintColor = UIColor(.blue)
-                UISegmentedControl.appearance().setTitleTextAttributes([.foregroundColor: UIColor.black, .font: UIFont.systemFont(ofSize: 10)], for: .normal)
-                UISegmentedControl.appearance().setTitleTextAttributes([.foregroundColor: UIColor.white], for: .selected)
-            }
-    }
-    
 }
 
 #Preview {
